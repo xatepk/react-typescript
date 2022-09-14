6d56e2ef07a0c5b367792cd3c06ca0fa71d07063
xatepk <kkhatya@gmail.com>
2022-09-14 15:49:04 +0400

    first commit

---
+++ src/components/Product.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { IProduct } from "../models";
+
+interface ProductProps {
+    product: IProduct
+}
+
+export function Product({ product }: ProductProps) {
+    return(
+        <div className="border py-2 px-4 rounded flex flex-col items-center mb-2">
+            <img src={product.image} className="w-1/6" alt={product.title} />
+            <p>{product.title}</p>
+            <p className="font-bold">{product.price}</p>
+        </div>
+    )
+}
