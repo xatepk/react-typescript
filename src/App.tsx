c2edef5d7e14fa774761cfc9208e22507e51157a
xatepk <kkhatya@gmail.com>
2022-09-21 14:04:00 +0400

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ import { CreateProduct } from './components/CreateProduct';
 
 function App() {
   const { products, loading, error, addProduct } = useProducts();
-  const [modal, setModal] = useState(true);
+  const [modal, setModal] = useState(false);
 
   const createHandler = (product:IProduct) => {
     setModal(false);
@@ -22,9 +22,14 @@ function App() {
       {error && <ErrorMessage error={error} />}
       {products.map((product: IProduct) => <Product product={product} key={product.id} />)}
 
-      {modal && <Modal title="Create new product" >
+      {modal && <Modal title="Create new product" onClose={() => setModal(false)}>
         <CreateProduct onCreate={createHandler} />
       </Modal>}
+
+      <button className='fixed bottom-5 right-5 
+              rounded-full bg-red-700 text-white text-2xl px-4 py-2'
+              onClick={() => setModal(true)}
+      >+</button>
     </div>
   );
 }
--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -2,14 +2,15 @@ import React from "react";
 
 interface ModalProps {
     children: React.ReactNode,
-    title: string
+    title: string,
+    onClose: () => void
 }
 
-export function Modal({ children, title }: ModalProps) {
+export function Modal({ children, title, onClose }: ModalProps) {
     return(
         <>
         <div className='fixed bg-black/50 top-0 right-0 left-0 bottom-0'
-        />
+        onClick={onClose}/>
         <div
             className='w-[550px] p-5 rounded bg-white absolute top-1/2 left-1/2 -translate-x-1/2 translate-y-1/2'
         >
