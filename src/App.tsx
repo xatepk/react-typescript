1a24ca3461419468be70a97abc181a21c66283d7
xatepk <kkhatya@gmail.com>
2022-09-21 15:31:27 +0400

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,21 @@
-import React, { useState } from 'react';
-import {Product} from './components/Product'
-import { useProducts } from './hooks/products';
-import { IProduct } from './models';
-import { Loader } from './components/Loader';
-import { ErrorMessage } from './components/ErrorMessage';
-import { Modal } from './components/Modal';
-import { CreateProduct } from './components/CreateProduct';
+import React from 'react';
+import {Route, Routes} from 'react-router-dom';
+import { ProductPages } from './pages/ProductPages';
+import { AboutPage } from './pages/AboutPage';
+import { Navigation } from './components/Navigation';
 
 function App() {
-  const { products, loading, error, addProduct } = useProducts();
-  const [modal, setModal] = useState(false);
+  return(
+    <>
+    <Navigation />
+    <Routes>
+      <Route path='/' element={<ProductPages />}></Route>
+      <Route path='/about' element={<AboutPage />}></Route>
+    </Routes>
+    </>
 
-  const createHandler = (product:IProduct) => {
-    setModal(false);
-    addProduct(product);
-  }
-
-  return (
-    <div className="container mx-auto max-w-2xl pt-5">
-      {loading && <Loader />}
-      {error && <ErrorMessage error={error} />}
-      {products.map((product: IProduct) => <Product product={product} key={product.id} />)}
-
-      {modal && <Modal title="Create new product" onClose={() => setModal(false)}>
-        <CreateProduct onCreate={createHandler} />
-      </Modal>}
-
-      <button className='fixed bottom-5 right-5 
-              rounded-full bg-red-700 text-white text-2xl px-4 py-2'
-              onClick={() => setModal(true)}
-      >+</button>
-    </div>
-  );
+  )
+  
 }
 
 export default App;
---
+++ src/pages/AboutPage.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function AboutPage() {
+    return(
+        <div>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi eaque exercitationem quis neque voluptatibus illum. Ullam dolor aspernatur numquam ab vitae consectetur quae a adipisci culpa veritatis quia, facere enim earum! Voluptatibus obcaecati soluta ut velit ullam. Velit, eaque consequatur ducimus optio, reiciendis ratione saepe illum cupiditate fugit maxime doloremque.
+        </div>
+    )
+}
